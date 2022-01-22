f5dce9b300bf50bdf6139ae5d624e0d9a33f5b1d
Mannan Javid <mannanjavid@gmail.com>
2022-01-22 12:40:20 -0500

    Initialized app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'yourlifeinblocks',
+    appId: '1:1036227969189:web:4b424e3df9c28e0a5b0d4c',
+    storageBucket: 'yourlifeinblocks.appspot.com',
+    apiKey: 'AIzaSyAZRnkXk7VDT4K6q6ZepOijrxcvPWZqJ7Q',
+    authDomain: 'yourlifeinblocks.firebaseapp.com',
+    messagingSenderId: '1036227969189',
+  },
   production: true
 };
