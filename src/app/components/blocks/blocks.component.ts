dd7bfd51fb9ee5dad3becdf549e9e5561b2c91d1
Mannan Javid <mannanjavid@gmail.com>
2022-01-25 23:30:55 -0500

    Add scrolling, block ids, row highlight

--- src/app/components/blocks/blocks.component.ts
+++ src/app/components/blocks/blocks.component.ts
@@ -3,6 +3,9 @@ import { Store } from '@ngrx/store';
 import { Subject, takeUntil, tap } from 'rxjs';
 import * as blocks from 'src/app/models/blocks.model';
 import * as blocksSelectors from 'src/app/state/blocks.selectors';
+import * as pah from 'src/app/helpers/page.helpers';
+import * as dth from 'src/app/helpers/datetime.helpers';
+import { format } from 'date-fns';
 
 @Component({
   selector: 'app-blocks',
@@ -10,9 +13,11 @@ import * as blocksSelectors from 'src/app/state/blocks.selectors';
   styleUrls: ['./blocks.component.scss']
 })
 export class BlocksComponent implements OnInit {
-  blocks = [] as blocks.weeksByYear;
+  weeksByYear = [] as blocks.weeksByYear;
+  activeBlockId!:string;
+  viewHasInit!:boolean;
   private _unsubscribe$ = new Subject<void>();
-  
+
   constructor(
     private store: Store
   ) { }
@@ -20,10 +25,31 @@ export class BlocksComponent implements OnInit {
   ngOnInit(): void {
     this.store.select(blocksSelectors.getWeeksByYear)
       .pipe(takeUntil(this._unsubscribe$),tap(blocks => console.log()))
-      .subscribe((blocks) => this.blocks = blocks);
+      .subscribe((blocks) => {
+        this.weeksByYear = blocks;
+        this.setActiveBlock();
+        if (this.viewHasInit) {
+          pah.scrollToBlock(this.activeBlockId, 'blocks', {x: 100, y: 250}, true);
+        }
+      });
   }
 
   ngAfterViewInit() {
+    if (this.activeBlockId) {
+      pah.scrollToBlock(this.activeBlockId, 'blocks', {x: 100, y: 250}, true);
+    } else {
+      this.viewHasInit = true;
+    }
+  }
+
+  setActiveBlock() {
+    const thisWeek = dth.getStartOfWeek();
+    const year = parseInt(format(thisWeek, 'y'));
+    this.weeksByYear[year].forEach(week => {
+      if (format(thisWeek, 'MM/dd/yyyy') === format(week.date, 'MM/dd/yyyy')) {
+        this.activeBlockId = `${year}_block_${week.blockNo}`;
+      }
+    })
   }
 
   getObjectKeys(obj: any): string[] {
