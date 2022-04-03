eaaa61b149b3a5a8ec7d980108f2789dfb3488e5
Mannan Javid <mannanjavid@gmail.com>
2022-04-03 14:37:53 -0400

    Creating first attempt at forms.

--- src/app/components/user-form/user-form.component.ts
+++ src/app/components/user-form/user-form.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Modes {
+  LOGIN = 'LOGIN',
+  SIGNUP = 'SIGNUP',
+}
 @Component({
   selector: 'app-user-form',
   templateUrl: './user-form.component.html',
   styleUrls: ['./user-form.component.scss']
 })
 export class UserFormComponent implements OnInit {
+  mode = Modes.SIGNUP;
+  LOGIN = Modes.LOGIN;
+  SIGNUP = Modes.SIGNUP;
+  // form values
+  email = '';
+  password = '';
+  fname = '';
+  lname = '';
+  dob = new Date().toISOString().substring(0,10);
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changeMode(mode: Modes) {
+    this.mode = mode;
+  }
+
+  login() {
+    console.log('loggingin!');
+  }
+
+  signup() {
+    console.log('signing up!');
   }
 
 }
